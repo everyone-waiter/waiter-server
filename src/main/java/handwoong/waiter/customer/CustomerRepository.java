7c7b70e1d53827efdfa303ee914661e4b456d1ef
handwoong <handwoong@gmail.com>
2023-03-02 18:34:20 +0900

    feat: 고객 repository 인터페이스 추가

---
+++ src/main/java/handwoong/waiter/customer/CustomerRepository.java
@@ -0,0 +1,22 @@
+package handwoong.waiter.customer;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CustomerRepository {
+	Optional<Customer> findByWaitingNumber(Long waitingNumber);
+
+	Optional<Customer> findFirstByOrderByWaitingNumberDesc();
+
+	Optional<Customer> findFirstByOrderByWaitingNumberAsc();
+
+	Optional<Customer> deleteById(String id);
+
+	Optional<Customer> findById(String id);
+
+	<S extends Customer> S save(S entity);
+
+	long count();
+
+	List<Customer> findAll();
+}
