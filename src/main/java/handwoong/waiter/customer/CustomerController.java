ef23e5d27a6cee94da123ea8a5b911b0db7f3728
handwoong <handwoong@gmail.com>
2023-03-02 15:00:56 +0900

    feat: 웨이팅 등록 View 추가

--- src/main/java/handwoong/waiter/customer/CustomerController.java
+++ src/main/java/handwoong/waiter/customer/CustomerController.java
@@ -10,7 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 @Controller
 @RequestMapping("/customers/waiting")
 public class CustomerController {
-	@GetMapping("/")
+	@GetMapping
 	public String waitingList() {
 		return "customers/index";
 	}
