b8211a5f5a61ca189e9a4ad4cdb3dbd8b3db9808
handwoong <handwoong@gmail.com>
2023-03-02 14:14:48 +0900

    chore: 초기설정

---
+++ src/main/java/handwoong/waiter/customer/CustomerController.java
@@ -0,0 +1,17 @@
+package handwoong.waiter.customer;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+@RequestMapping("/customers/waiting")
+public class CustomerController {
+	@GetMapping("/")
+	public String waitingList() {
+		return "customers/index";
+	}
+}
